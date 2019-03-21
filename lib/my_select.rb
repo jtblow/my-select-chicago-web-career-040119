c6647a2c39bf66cd4ade3b9e3af623945f31bcb2
creative-zombie-4458 <jtbartlow@gmail.com>
2019-03-21 20:16:02 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,10 +1,12 @@
-def my_select(collection)
- i = 0 
- select = []
- while i < collection.length
- i+=1
-if i % 2 == 0 
-  select << i
-end
-select
+
+def my_select(array)
+  i = 0
+  select = []
+  while i < array.length
+    if yield(array[i])
+      select << array[i]
+    end
+    i+=1
+  end
+  select
 end
